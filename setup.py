0f3acd4abb83c32db1fc266de38b8298b2a68621
lorenzoo <lorenzo.o@cquest.io>
2021-12-03 14:13:21 +0100

    Pimp version to 0.1.11

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def parse_requirements(filename):
 
 setup(
     name="data_validation_module",
-    version="0.1.10",
+    version="0.1.11",
     description="A tool with different functions for the data validation",
     author="Lorenzo Olivier",
     author_email="lorenzo.o@seqana.com",
